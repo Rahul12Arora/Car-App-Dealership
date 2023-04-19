e999cc1cfaffa0703908aa3df8c2ea117da81a68
Rahul Arora <108695777+Rahul12Arora@users.noreply.github.com>
2023-04-19 20:45:41 +0530

    upd

--- car-backend/index.js
+++ car-backend/index.js
@@ -4,15 +4,21 @@ const port = process.env.PORT || 5001;
 const express =  require('express');
 const app = express();
 
+const mongoDB =require("./db");
+mongoDB();
 
-app.get('/', (req, res) => {
-    // res.sendStatus(200);
-    res.send('ching chong');
-    // res.status(200).send('ching chong');
-    // res.status(200).json({message:"chong ching")}
-    // res.json() sends a default success code as we eliminated the status request
-    // res.download('index.js'); can be used to send data to user that he can download, provide file path as argument
+app.use(express.json());
+app.post("/api/signup",(req,res)=>{
+    res.send("user created successfully");
 })
+// app.get('/', (req, res) => {
+//     // res.sendStatus(200);
+//     res.send('ching chong');
+//     // res.status(200).send('ching chong');
+//     // res.status(200).json({message:"chong ching")}
+//     // res.json() sends a default success code as we eliminated the status request
+//     // res.download('index.js'); can be used to send data to user that he can download, provide file path as argument
+// })
 
 app.listen(port,()=>{
     console.log('server started');
